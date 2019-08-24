ea94aaeea6863423577d0df0679e9ff4d0f86fee
Julio Muller <juliodolago@gmail.com>
2019-08-24 19:16:49 -0300

    Fixed non-closing tags BODY and HTML and performed minor linting thoughout the code

--- Exercicio01/src/java/exercicio01/SEPT.java
+++ Exercicio01/src/java/exercicio01/SEPT.java
@@ -41,17 +41,34 @@ public class SEPT extends HttpServlet {
         "http://www.iaa.ufpr.br",
         "http://www.engenhariadesoftware.ufpr.br:28080/engenhariadesoftware/"    
     };
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        
         response.setContentType("text/html;charset=UTF-8");
-
+        String[] imgCurso = {
+            request.getContextPath() + "/imgs/01acs.png",
+            request.getContextPath() + "/imgs/02pg.png",
+            request.getContextPath() + "/imgs/03ads.png",
+            request.getContextPath() + "/imgs/04ci.png",
+            request.getContextPath() + "/imgs/05gq.png",
+            request.getContextPath() + "/imgs/06gp.png",
+            request.getContextPath() + "/imgs/07l.png",
+            request.getContextPath() + "/imgs/08ni.png",
+            request.getContextPath() + "/imgs/09pc.png",
+            request.getContextPath() + "/imgs/10s.png",
+            request.getContextPath() + "/imgs/11b.png",
+            request.getContextPath() + "/imgs/12eia.png",
+            request.getContextPath() + "/imgs/13ees.png"
+        };
+        
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<meta charset=\"UTF-8\" />");
             out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
-
             out.println("<title>SEPT | Web 2</title>");
             out.println("<link rel=\"stylesheet\" href=\"" + request.getContextPath()+ "/css/bootstrap.min.css\">");
             out.println("</head>");
@@ -61,9 +78,9 @@ public class SEPT extends HttpServlet {
             out.println("<div style=\"background: rgb(255, 255, 255, 0.1);\">");
             out.println("<div class=\"container jumbotron text-white font-weight-bold\" style=\"background: rgb(0, 0, 0, 0);\">");
             out.println("<h1 class=\"display-4\" style=\"font-family:verdana;font-size:40px;color:blue;\">SEPT - Setor de Educação "
-                    + "Profissional e tecnológica</h1><br/><br/>");
+                + "Profissional e tecnológica</h1><br/><br/>");
             out.println("<a href=\"http://www.sept.ufpr.br/portal/\"><p class=\"font-weight-light\" style=\"font-family:arial;"
-                    + "color:black;font-size:20px;text-align:center\">Página do setor</p></a>");
+                + "color:black;font-size:20px;text-align:center\">Página do setor</p></a>");
             out.println("<hr class=\"d-block my-4 bg-white\" style=\"height: 2px;\" />");
             out.println("<div class=\"text-center\">");
             out.println("<a href=\"" + request.getContextPath() + "/tads.jsp\" class=\"btn btn-outline-dark\">Meu TADS</a>");
@@ -72,30 +89,13 @@ public class SEPT extends HttpServlet {
             out.println("</div>");
             out.println("</div>");
             out.println("</header>");
-                String[] imgCurso = {
-                request.getContextPath() + "/imgs/01acs.png",
-                request.getContextPath() + "/imgs/02pg.png",
-                request.getContextPath() + "/imgs/03ads.png",
-                request.getContextPath() + "/imgs/04ci.png",
-                request.getContextPath() + "/imgs/05gq.png",
-                request.getContextPath() + "/imgs/06gp.png",
-                request.getContextPath() + "/imgs/07l.png",
-                request.getContextPath() + "/imgs/08ni.png",
-                request.getContextPath() + "/imgs/09pc.png",
-                request.getContextPath() + "/imgs/10s.png",
-                request.getContextPath() + "/imgs/11b.png",
-                request.getContextPath() + "/imgs/12eia.png",
-                request.getContextPath() + "/imgs/13ees.png"
-            };
-            
             out.println("<div class=\"container text-center font-family\">");
             out.println("<h2>Cursos oferecidos no setor</h2>");
             out.println("<main class=\"mt-5\"");
             out.println("<br><h3>Cursos técnicos</h3><br/>");
-            int j = 0;
             out.println("<div class=\"row justify-content-center\">");
             out.println("<div class=\"card-deck\">");
-            for (int i = 0; i < curso.length; i++) {
+            for (int i = 0, j = 0; i < curso.length; i++) {
                 if (curso[i].equals("Análise e Desenvolvimento de Sistemas")) {
                     j = 0;
                     out.println("</div></div>"); // fim do <div row>
@@ -120,13 +120,13 @@ public class SEPT extends HttpServlet {
                 }
                 out.println("<div class=\"card sm-3 m-4\" style=\"width:12rem;\">");
                 out.println("<img class=\"card-img-top\" src=\"" + imgCurso[i] +"\" "
-                        + "alt=\"Miniatura curso " + curso[i] + "\">");
+                    + "alt=\"Miniatura curso " + curso[i] + "\">");
                 out.println("<div class=\"card-body\">");
                     out.println("<h5 class=\"card-title\">" + curso[i] + "</h5>");
                 out.println("</div>");
                 out.println("<div class=\"card-footer\">");
                 out.println("<a href=\"" + linkCurso[i] + "\" class=\"card-link\">"
-                        + "Página do Curso</a>");
+                    + "Página do Curso</a>");
                 out.println("</div>");
                 out.println("</div>");       
                 j++;
@@ -135,7 +135,8 @@ public class SEPT extends HttpServlet {
             out.println("</div>");
             out.println("</main>");
             out.println("</div>");
-
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
@@ -177,5 +178,4 @@ public class SEPT extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
