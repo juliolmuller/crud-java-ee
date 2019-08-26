f4324faaebbf33bb9c859833192a2dec585cfe1b
Julio Muller <juliodolago@gmail.com>
2019-08-25 22:04:40 -0300

    Finished 'CadastrarUsuarioServlet' to handle AJAX requests

--- Exercicio02/src/java/exercicio/CadastrarUsuarioServlet.java
+++ Exercicio02/src/java/exercicio/CadastrarUsuarioServlet.java
@@ -3,6 +3,8 @@ package exercicio;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,82 +15,128 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "CadastrarUsuarioServlet", urlPatterns = {"/cadastrar-usuario"})
 public class CadastrarUsuarioServlet extends HttpServlet {
 
-    protected void processRequest(
+    protected boolean validateRequest(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws IOException  {
+        
+        // Configurar input e output
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        
+        // Validar se usuário está logado
+        if (request.getSession(false) == null) {
+            response.sendRedirect(request.getContextPath());
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    protected void doGet(
         HttpServletRequest request,
         HttpServletResponse response
     ) throws ServletException, IOException {
         
-        // Recuperar sessão ativa ou redirecionar para tela de login
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            response.sendRedirect(request.getContextPath());
+        // Validar session e configurar entrada e saída de dados
+        if (!validateRequest(request, response)) {
             return;
         }
-        Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+        
+        // Recuperar dados de sessão
+        HttpSession session = request.getSession(false);
+        List<Usuario> listaUsuarios = (ArrayList<Usuario>) session.getAttribute("listaUsuarios");
+        
+        // Se é a primeira vez que se está acessando a servlet, intanciar lista de usuários padrão
+        if (listaUsuarios == null) {
+            listaUsuarios = new ArrayList<>();
+            listaUsuarios.add(new Usuario("André Antunes", "AANTUNES", "IS2adm"));
+            listaUsuarios.add(new Usuario("Aurélio Matsunaga", "AMATSUNA", "GOisC00l"));
+            listaUsuarios.add(new Usuario("Cassiano Vidal", "CVIDAL", "humble-boy"));
+            listaUsuarios.add(new Usuario("Júlio Müller", "JMULLER", "qwerty123"));
+            listaUsuarios.add(new Usuario("Wesley Caetano", "WCAETANO", "senha1234"));
+            session.setAttribute("listaUsuarios", listaUsuarios);
+        }
+        try (PrintWriter out = response.getWriter()) {
+            out.print("[");
+            for (int i = 0; i < listaUsuarios.size(); i++) {
+                out.print("{");
+                out.print("\"nome\":\"" + listaUsuarios.get(i).getNome() + "\",");
+                out.print("\"login\":\"" + listaUsuarios.get(i).getLogin() + "\",");
+                out.print("\"senha\":\"" + listaUsuarios.get(i).getSenha() + "\"");
+                out.print("}");
+                if (i < listaUsuarios.size() - 1) {        
+                    out.print(",");
+                }
+            }
+            out.print("]");
+        }
+        
+    }
+
+    @Override
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        
+        // Validar session e configurar entrada e saída de dados
+        if (!validateRequest(request, response)) {
+            return;
+        }
+        
+        // Salvar os parâmetros enviados na requisição
+        String nome = request.getParameter("nome");
+        String login = request.getParameter("login");
+        String senha = request.getParameter("senha");
+        
+        // Validar se todos os parâmetros foram enviados
+        List<String> erros = new ArrayList<>();
+        System.out.print(nome + " => Júlio");
+        if (nome == null) {
+            erros.add("O campo NOME é obrigatório.");
+        }
+        if (login == null) {
+            erros.add("O campo LOGIN é obrigatório.");
+        }
+        if (senha == null) {
+            erros.add("O campo SENHA é obrigatório.");
+        }
         
         // Montar resposta ao usuário
-        response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<meta charset=\"UTF-8\" />");
-            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
-            out.println("<title>Web 2 :: Exercício 02</title>");
-            out.println("<link rel=\"stylesheet\" href=\"css/bootstrap.min.css\">");
-            out.println("<link rel=\"stylesheet\" href=\"css/login-styles.css\" />");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<div class=\"container\">");
-            out.println("<h1>Servlet CadastrarUsuarioServlet (logado como " 
-                + usuario.getLogin() + ", "
-                + usuario.getNome() + ", " 
-                + usuario.getSenha() + ")"
-                + "</h1>");
-            out.println("<a href=\"" + request.getContextPath() + "/logout\">Logout</a>");
-            out.println("</div>");
-            out.println("</body>");
-            out.println("</html>");
+            if (erros.size() > 0) {
+                response.setStatus(422);
+                out.print("{");
+                out.print("\"status\":\"error\",");
+                out.print("\"messages\":[");
+                for (int i = 0; i < erros.size(); i++) {
+                    out.print("\"" + erros.get(i) + "\"");
+                    if (i < erros.size() - 1) {        
+                        out.print(",");
+                    }
+                }
+                out.print("]}");
+            } else {
+                HttpSession session = request.getSession(false);
+                List<Usuario> listaUsuarios = (ArrayList<Usuario>) session.getAttribute("listaUsuarios");
+                Usuario usuario = new Usuario(nome, login, senha);
+                listaUsuarios.add(usuario);
+                session.setAttribute("listaUsuarios", listaUsuarios);
+                out.print("{");
+                out.print("\"status\":\"success\",");
+                out.print("\"data\":{");
+                out.print("\"nome\":\"" + usuario.getNome() + "\",");
+                out.print("\"login\":\"" + usuario.getLogin() + "\",");
+                out.print("\"senha\":\"" + usuario.getSenha() + "\"");
+                out.print("}}");
+            }
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
