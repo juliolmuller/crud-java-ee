14bdee8a860b829d1aba938d378361c54339ee57
Julio Muller <juliodolago@gmail.com>
2019-09-07 17:14:01 -0300

    Started up 'Exercicio04'

---
+++ Exercicio04/src/java/exercicio/Erro.java
@@ -0,0 +1,84 @@
+package exercicio;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Erro", urlPatterns = {"/Erro"})
+public class Erro extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String errMsg = (String)request.getAttribute("errMsg");
+        String page = (String)request.getAttribute("page");
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset=\"UTF-8\" />");
+            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
+            out.println("<title>Web 2 :: Exercício 04</title>");
+            out.println("<link rel=\"stylesheet\" href=\"" + request.getContextPath() + "/css/bootstrap.min.css\">");
+            out.println("<link rel=\"stylesheet\" href=\"" + request.getContextPath() + "/css/login-styles.css\" />");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<div class=\"wrapper fade-in-down\">");
+            out.println("<div id=\"form-content\">");
+            out.println("<div class=\"fade-in first\">");
+            out.println("<img src=\"" + request.getContextPath() + "/img/uncheck-icon.png\" id=\"icon\" alt=\"Ícone de erro\" /></div>");
+            out.println("<h3 class=\"mb-5 fade-in third text-danger\">" + errMsg + "</h3>");
+            out.println("<div id=\"form-footer\">");
+            out.println("<a href=\"" + page + "/\" class=\"underline-hover\">Voltar para a tela de login</a>");
+            out.println("</div></div></div>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
