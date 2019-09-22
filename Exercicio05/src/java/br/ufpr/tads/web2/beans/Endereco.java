8258a10f8ed8d4790be08a79cea422dab2f4f491
Julio Muller <juliodolago@gmail.com>
2019-09-22 15:07:17 -0300

    Created beans Cliente & Endereco

---
+++ Exercicio05/src/java/br/ufpr/tads/web2/beans/Endereco.java
@@ -0,0 +1,54 @@
+package br.ufpr.tads.web2.beans;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+
+    private String rua;
+    private int numero;
+    private String cep;
+    private String cidade;
+    private String uf;
+
+    public Endereco() {}
+
+    public String getRua() {
+        return this.rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return this.uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
