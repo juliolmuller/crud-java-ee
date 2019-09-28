ff77f70e36e02290beaca4f4b1a9b227b9e814d9
Julio Muller <juliodolago@gmail.com>
2019-09-28 07:35:17 -0300

    Started up assignment prototype

---
+++ Trabalho/app/index.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict'
+
+  angular.module('mainApp', ['ngRoute'])
+  angular.module('clienteApp', ['ngRoute'])
+  angular.module('gerenteApp', ['ngRoute'])
+  angular.module('funcionarioApp', ['ngRoute'])
+
+})()
