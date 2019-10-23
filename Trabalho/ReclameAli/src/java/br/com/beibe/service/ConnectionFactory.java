d85ee43975bf8407b733eb217cc5c422193bbfec
Julio Muller <juliodolago@gmail.com>
2019-10-22 22:06:04 -0300

    Added the option to set auto-commit database option

--- Trabalho/ReclameAli/src/java/br/com/beibe/service/ConnectionFactory.java
+++ Trabalho/ReclameAli/src/java/br/com/beibe/service/ConnectionFactory.java
@@ -2,20 +2,19 @@ package br.com.beibe.service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public abstract class ConnectionFactory {
 
     private static final String PROPS_FILE = "/db.properties";
-    private static Properties props;
+    private static final Properties PROPS = new Properties();
 
     static {
         try (InputStream is = ConnectionFactory.class.getResourceAsStream(PROPS_FILE)) {
-            props = new Properties();
-            props.load(is);
+            PROPS.load(is);
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
@@ -23,13 +22,15 @@ public abstract class ConnectionFactory {
 
     private ConnectionFactory() {}
 
-    public static Connection getConnection() throws SQLException {
-        String dbUrl = props.getProperty("DB_URL")
-            + props.getProperty("DB_HOST") + ":"
-            + props.getProperty("DB_PORT") + "/"
-            + props.getProperty("DB_SCHEMA");
-        String dbUser = props.getProperty("DB_USER");
-        String dbPassword = props.getProperty("DB_PASSWORD");
-        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    public static Connection getConnection(boolean autoCommit) throws SQLException {
+        String dbUrl = PROPS.getProperty("DB_URL")
+            + PROPS.getProperty("DB_HOST") + ":"
+            + PROPS.getProperty("DB_PORT") + "/"
+            + PROPS.getProperty("DB_SCHEMA");
+        String dbUser = PROPS.getProperty("DB_USER");
+        String dbPassword = PROPS.getProperty("DB_PASSWORD");
+        Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        conn.setAutoCommit(autoCommit);
+        return conn;
     }
 }
