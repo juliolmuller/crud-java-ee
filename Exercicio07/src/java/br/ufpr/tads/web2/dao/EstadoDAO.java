438b05f4cd1d2ad849ecdbceb5049a7cdb7c747a
Julio Muller <juliodolago@gmail.com>
2019-10-27 22:34:22 -0300

    Created DAO for 'cidade' and 'estado'

---
+++ Exercicio07/src/java/br/ufpr/tads/web2/dao/EstadoDAO.java
@@ -0,0 +1,34 @@
+package br.ufpr.tads.web2.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
+import br.ufpr.tads.web2.beans.Estado;
+
+public abstract class EstadoDAO {
+
+    private static final String TABELA = "tb_estado";
+
+    public static List<Estado> listar() {
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            List<Estado> estados = new ArrayList<>();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                "SELECT id_estado, nome_estado, sigla_estado FROM " + TABELA + ";"
+            );
+            while (rs.next()) {
+                Estado estado = new Estado();
+                estado.setId(rs.getInt("id_estado"));
+                estado.setNome(rs.getString("nome_estado"));
+                estado.setSigla(rs.getString("sigla_estado"));
+                estados.add(estado);
+            }
+            return estados;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
