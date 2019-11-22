b21dc0175fd8a9586e3247bbfe75df3fb955be95
Julio Muller <juliodolago@gmail.com>
2019-11-22 00:19:46 -0300

    Changed bean for tickets status

--- Trabalho/ReclameAli/src/java/br/com/beibe/beans/TicketStatus.java
+++ Trabalho/ReclameAli/src/java/br/com/beibe/beans/TicketStatus.java
@@ -1,5 +1,36 @@
 package br.com.beibe.beans;
 
 public enum TicketStatus {
-    OPEN, CLOSED
+    OPEN(1, "Aberto"),
+    CLOSED(2, "Fechado");
+
+    private final int id;
+    private final String name;
+
+    private TicketStatus(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static TicketStatus ofId(int id) {
+        TicketStatus[] ts =  values();
+        for (int i = 0; i < ts.length; i++) {
+            if (ts[i].id == id)
+                return ts[i];
+        }
+        return null;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
