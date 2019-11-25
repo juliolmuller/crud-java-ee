11b02b67500e0c43aff369d5f8caa7b51d1c172c
Julio Muller <juliodolago@gmail.com>
2019-11-24 23:26:45 -0300

    Created end-point to process requests related to tickets

--- Trabalho/ReclameAli/src/java/br/com/beibe/facade/TicketFacade.java
+++ Trabalho/ReclameAli/src/java/br/com/beibe/facade/TicketFacade.java
@@ -5,8 +5,13 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import br.com.beibe.beans.Ticket;
+import br.com.beibe.beans.TicketMessage;
+import br.com.beibe.beans.TicketStatus;
+import br.com.beibe.beans.TicketType;
 import br.com.beibe.beans.User;
 import br.com.beibe.dao.TicketDAO;
+import br.com.beibe.dao.TicketMessageDAO;
+import br.com.beibe.dao.TicketTypeDAO;
 
 public abstract class TicketFacade {
 
@@ -24,6 +29,14 @@ public abstract class TicketFacade {
     public static Ticket find(Long id) {
         return TicketDAO.find(id);
     }
+    
+    public static TicketType findType(long id) {
+        return TicketTypeDAO.find(id);
+    }
+    
+    public static TicketStatus findStatus(int id) {
+        return TicketStatus.ofId(id);
+    }
 
     public static void save(Ticket ticket) {
         try {
@@ -31,6 +44,14 @@ public abstract class TicketFacade {
                 TicketDAO.insert(ticket);
             else
                 TicketDAO.update(ticket);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public static void save(TicketMessage message) {
+        try {
+            TicketMessageDAO.insert(message);
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
--- Trabalho/ReclameAli/src/java/br/com/beibe/servlet/ApiTicketsServlet.java
+++ Trabalho/ReclameAli/src/java/br/com/beibe/servlet/ApiTicketsServlet.java
@@ -2,45 +2,25 @@ package br.com.beibe.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
-import br.com.beibe.beans.Category;
-import br.com.beibe.beans.Product;
+import br.com.beibe.beans.Ticket;
+import br.com.beibe.beans.TicketMessage;
+import br.com.beibe.beans.User;
 import br.com.beibe.beans.ValError;
 import br.com.beibe.facade.ProductFacade;
+import br.com.beibe.facade.TicketFacade;
 
-@WebServlet(name = "ProductsAPI", urlPatterns = {"/api/products"})
-public class ApiProductsServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(
-        HttpServletRequest request,
-        HttpServletResponse response
-    ) throws ServletException, IOException {
-        Product product;
-        try {
-            Long id = Long.parseLong(request.getParameter("id"));
-            product = ProductFacade.find(id);
-        } catch (NumberFormatException ex) {
-            product = null;
-        }
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json");
-        try (PrintWriter out = response.getWriter()) {
-            Gson json = new Gson();
-            if (product != null) {
-                out.print(json.toJson(product));
-            } else {
-                List<Product> products = ProductFacade.listAll();
-                out.print(json.toJson(products));
-            }
-        }
-    }
+@WebServlet(name = "TicketsAPI", urlPatterns = {"/api/tickets"})
+public class ApiTicketsServlet extends HttpServlet {
 
     @Override
     protected void doPost(
@@ -51,30 +31,35 @@ public class ApiProductsServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
+        Ticket ticket;
+        try {
+            Long id = Long.parseLong(request.getParameter("id"));
+            ticket = TicketFacade.find(id);
+        } catch (NumberFormatException ex) {
+            ticket = null;
+        }
         try (PrintWriter out = response.getWriter()) {
             Gson json = new Gson();
-            Product product = extractData(request);
             List<ValError> errors;
             switch (action) {
-                case "delete":
-                    if (product.getId() == null) {
+                case "message":
+                    TicketMessage message = extractMessageData(request);
+                    errors = message.validate();
+                    if (errors.isEmpty()) {
+                        TicketFacade.save(message);
+                        out.print(json.toJson(message));
+                    } else {
                         response.setStatus(422);
-                        out.print(json.toJson(new ValError("error", "Nenhum ID informado para exclusão")));
-                    } else if (!ProductFacade.listTickets(product).isEmpty()) {
-                        response.setStatus(422);
-                        out.print(json.toJson(new ValError("error", "Há atendimentos associados a esse produto")));
-                    } else if (!ProductFacade.delete(product)) {
-                        response.setStatus(422);
-                        out.print(json.toJson(new ValError("error", "Falha ao excluir produto")));
-                    } else
-                        out.print(json.toJson(new ValError("success", "Produto excluído com sucesso")));
+                        out.print(json.toJson(errors));
+                    }
                     return;
                 case "new":
                 case "update":
-                    errors = product.validate();
+                    ticket = extractTicketData(request);
+                    errors = ticket.validate();
                     if (errors.isEmpty()) {
-                        ProductFacade.save(product);
-                        out.print(json.toJson(product));
+                        TicketFacade.save(ticket);
+                        out.print(json.toJson(ticket));
                     } else {
                         response.setStatus(422);
                         out.print(json.toJson(errors));
@@ -84,31 +69,31 @@ public class ApiProductsServlet extends HttpServlet {
         }
         response.sendError(404);
     }
+    
+    private TicketMessage extractMessageData(HttpServletRequest request) {
+        TicketMessage message = new TicketMessage();
+        message.setMessage(request.getParameter("message"));
+        message.setSendDate(LocalDateTime.now());
+        message.setSender((User) request.getSession().getAttribute("userCredentials"));
+        message.setTicket(Long.parseLong(request.getParameter("id")));
+        return message;
+    }
 
-    private Product extractData(HttpServletRequest request) {
-        Product product = new Product();
+    private Ticket extractTicketData(HttpServletRequest request) {
+        Ticket ticket = new Ticket();
+        ticket.setType(TicketFacade.findType(Integer.parseInt(request.getParameter("type"))));
+        ticket.setStatus(TicketFacade.findStatus(Integer.parseInt(request.getParameter("status"))));
+        ticket.setProduct(ProductFacade.find(Long.parseLong(request.getParameter("product"))));
+        ticket.setOpeningDate(LocalDateTime.now());
+        ticket.setOpenBy((User) request.getSession().getAttribute("userCredentials"));
         try {
-            product.setId(Long.parseLong(request.getParameter("id")));
+            ticket.setId(Long.parseLong(request.getParameter("id")));
         } catch (NumberFormatException | NullPointerException ex) {
-            product.setId(null);
+            ticket.setId(null);
         }
-        product.setName(request.getParameter("name"));
-        product.setDescription(request.getParameter("description"));
-        try {
-            product.setWeight(Double.parseDouble(request.getParameter("weight")));
-        } catch (NumberFormatException | NullPointerException ex) {
-            product.setWeight(null);
-        }
-        product.setUtc(request.getParameter("utc_code"));
-        product.setEan(request.getParameter("ean_code"));
-        Category category = new Category();
-        try {
-            category.setId(Long.parseLong(request.getParameter("category")));
-        } catch (NullPointerException | NumberFormatException ex) {
-        } finally {
-            product.setCategory(category);
-        }
-
-        return product;
+        Set<TicketMessage> messages = new TreeSet<>();
+        messages.add(extractMessageData(request));
+        ticket.setMessages(messages);
+        return ticket;
     }
 }
